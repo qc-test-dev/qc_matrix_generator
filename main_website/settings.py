c555423fb4bdb1ae4d2a0529cd80ff968a4a3521
atamez6 <altmzg@gmail.com>
2025-06-10 21:57:06 -0600

    docker uat

--- main_website/settings.py
+++ main_website/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY','changeme')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', True)
 
-ALLOWED_HOSTS = ['200.57.172.7', 'localhost', '127.0.0.1','0.0.0.0']
+ALLOWED_HOSTS = ['200.57.172.7', 'localhost', '127.0.0.1','0.0.0.0','*']
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework.authentication.TokenAuthentication',
