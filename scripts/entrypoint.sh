81d04d65ab59f8b6659027546a2f3d089c70abb8
atamez6 <altmzg@gmail.com>
2025-06-10 22:11:58 -0600

    docker uat

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -22,7 +22,7 @@ fi
 
 # Iniciar uWSGI
 echo "🚀 Starting uWSGI server..."
-exec uwsgi --socket :8000 \
+exec uwsgi --socket :8080 \
      --master \
      --module main_website.wsgi \
      --enable-threads
