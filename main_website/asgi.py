5c1dfb9bf98b3a6e58ce07d0dc081f2db6bda41a
atamez6 <altmzg@gmail.com>
2025-07-21 19:12:32 -0600

    fix ws

--- main_website/asgi.py
+++ main_website/asgi.py
@@ -1,11 +1,13 @@
 import os
+import os
 from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
 from channels.auth import AuthMiddlewareStack
 import app.matrix.routing
 
-# 👇 Aquí usa el mismo settings que usas con manage_local.py
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'main_website.settings')
+# 🎯 Cambiar esta línea para usar la variable de entorno
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 
+    os.environ.get('DJANGO_SETTINGS_MODULE', 'main_website.settings.prod'))
 
 application = ProtocolTypeRouter({
     "http": get_asgi_application(),
