3962b9edc42b5d5d9ef9ed7cd4dbe527d40ff784
atamez6 <altmzg@gmail.com>
2025-07-22 01:54:39 -0600

    atamez

--- main_website/settings/prod.py
+++ main_website/settings/prod.py
@@ -2,21 +2,17 @@ from .base import *
 
 CSRF_TRUSTED_ORIGINS = [
     'http://200.57.172.7',
-    'http://200.57.172.7:80',
-    'http://200.57.172.7:8080',
-    'http://0.0.0.0',
-    'http://0.0.0.0:80',
     'http://localhost',
-    'http://localhost:80',
+    'http://127.0.0.1',
 ]
+
 
 ALLOWED_HOSTS = [
     '200.57.172.7',
     'localhost',
     '127.0.0.1',
-    '0.0.0.0',
-    '*',
 ]
+
 
 
 DEBUG = False
