dbb09137bd5d8ab77bfe6484171860200a66f9f1
atamez6 <altmzg@gmail.com>
2025-07-22 22:34:27 -0600

    socket fix

--- app/matrix/urls.py
+++ app/matrix/urls.py
@@ -17,7 +17,8 @@ urlpatterns = [
     path("api/guardar-validates/", api_guardar_validates, name="api_guardar_validates"),
     path('ajax/actualizar_estado/', views.actualizar_estado_caso, name='actualizar_estado_caso'),
     path('ajax/actualizar_nota/', views.actualizar_nota_caso, name='actualizar_nota_caso'),
-    
+    path('casos-json/', views.casos_json, name='casos_json'),
+
     path('ajax/actualizar_estado_validate/', views.actualizar_estado_validate, name='actualizar_estado_validate'),
     path('supermatriz/<int:supermatriz_id>/reporte_pdf/', views.generar_pdf_supermatriz, name='reporte_pdf_supermatriz'),
 ]   
