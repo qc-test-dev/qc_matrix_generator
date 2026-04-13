a5c85e56ccc2c09c0168dffb7e6ad659a6bdc502
lgabino <stroop94@gmail.com>
2026-04-13 11:45:56 -0600

    v13.2 nuevo campo en CasoDePrueba criterio aceptacion 10-04-2026

--- app/matrix/models.py
+++ app/matrix/models.py
@@ -210,6 +210,7 @@ class CasoDePrueba(models.Model):
     etiqueta=models.CharField(max_length=50, blank=True, null=True)
     tipo_usuario=models.CharField(max_length=70, blank=True, null=True)
     pasos=models.CharField(max_length=700,blank=True, null=True)
+    criterio_aceptacion = models.TextField(blank=True, null=True, verbose_name="Criterio de aceptación")
     def __str__(self):
         return f"{self.fase} - {self.caso_de_prueba[:30]}..."
 class Validate(models.Model):
